6607f307a2e03bca53dcee2b19fe64be8db65a5c
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2022-05-06 19:50:38 +0530

    IoT initial commit

---
+++ nodeMCU/examples/NODEMCU_SampleCodes/NODEMCU_SampleCodes/LED_D2_FADE/LED_D2_FADE.ino
@@ -0,0 +1,19 @@
+#define LED D2
+int val;
+void setup() 
+{
+  pinMode(LED,OUTPUT);
+}
+void loop() 
+{
+  for(val=0;val<1000;val+=50)
+  {
+    analogWrite(LED,val);
+    delay(100);
+  }
+  for(val=1000;val>1;val-=50)
+  {
+    analogWrite(LED,val);
+    delay(100);
+  }
+}
